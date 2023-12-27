c0fd8e2155f3802fecae698e8b4447133844725d
SaidBELAKDAIM <besaid60@gmail.com>
2023-12-27 20:32:30 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PresentationF from './index.jsx';
+
+const  root = ReactDOM.createRoot(document.getElementById('root'));
+  
+root.render(
+  
+   
+     <PresentationF winner={3}/>
+  
+)
